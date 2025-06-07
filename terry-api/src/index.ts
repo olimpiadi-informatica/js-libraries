96131013ddd40871ee4035eda0c229b97fb6dea7
Alessandro Bortolin <bortolin.alessandro@outlook.it>
2025-06-07 18:49:22 +0200

    Remove training-api and terry-api

--- terry-api/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { generateInput, abandonInput } from "./input";
-export * from "./score";
-export { getSubmission, getSubmissions, Submission } from "./submissions";
-export * from "./submit";
-export * from "./user";
--- training-api/src/fetch.ts
+++
@@ -1,9 +0,0 @@
-export function post(url: string, body: any, _setCookie?: boolean) {
-  return fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-}
